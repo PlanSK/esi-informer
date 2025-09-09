bf0d82d4c505e878ee9188cec1c03687cb7e176c
PlanSK <skapkanshchikov@gmail.com>
2025-09-09 23:16:54 +0300

    Add functions for dicts from json files and service functions.

--- region_checking.py
+++ region_checking.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 from get_space_locations_data import analyze_yaml_file_data
 from parse_id_data_from_yaml import parse_obj_structure
@@ -55,70 +56,89 @@ else:
     raise Exception("SDE files not found.")
 
 
-# Form region dict
-with open(os.path.join(ESI_DATA_DIR, JSON_REGIONS_LIST_FILENAME)) as json_file:
-    regions_list: list[dict] = json.load(json_file)
-regions_data_dict = {
-    region.get("id"): region.get("name") for region in regions_list
-}
-# Form solar system list
-with open(
-    os.path.join(ESI_DATA_DIR, JSON_SOLAR_SYSTEMS_LIST_FILENAME)
-) as json_file:
-    solar_system_list: list[dict] = json.load(json_file)
-solar_system_data_dict = {
-    system.get("name"): system for system in solar_system_list
-}
+def get_regions_data_dict() -> dict:
+    with open(
+        os.path.join(ESI_DATA_DIR, JSON_REGIONS_LIST_FILENAME)
+    ) as json_file:
+        regions_list: list[dict] = json.load(json_file)
+    regions_data_dict = {
+        region.get("id"): region.get("name") for region in regions_list
+    }
+    return regions_data_dict
 
-system_list = [
-    "98Q-8O",
-    "B-9C24",
-    "Gultratren",
-    "Isie",
-    "LSC4-P",
-    "M-MD3B",
-    "X-7OMU",
-    "Meunvon",
-    "Sujarento",
-    "Tararan",
-    "Ahbazon",
-    "A-1CON",
-    "Adeel",
-    "Aedald",
-    "Egbinger",
-    "G-0Q86",
-    "G8AD-C",
-    "Goinard",
-    "Lantorn",
-    "MN5N-X",
-    "PVH8-0",
-    "QFEW-K",
-    "Rakapas",
-    "Serpentis Prime",
-    "Turnur",
-    "Utopia",
-    "UW9B-F",
-    "XX9-WV",
-    "YZ-LQL",
-]
-default_color_prefix = "\033[0m"
 
-for system_name in system_list:
-    system_data = solar_system_data_dict.get(system_name)
-    region_name = regions_data_dict.get(system_data.get("region_id"))
+def get_constellations_data_dict() -> dict:
+    with open(
+        os.path.join(ESI_DATA_DIR, JSON_CONSTELLATIONS_LIST_FILENAME)
+    ) as json_file:
+        constellations_list: list[dict] = json.load(json_file)
+    constellations_data_dict = {
+        constellation.get("id"): constellation.get("name")
+        for constellation in constellations_list
+    }
+    return constellations_data_dict
 
-    if system_data.get("region_id") in CALDARI_REGION_ID_LIST:
-        first_color_prefix = "\033[1;36m"
-    elif system_data.get("region_id") in AMARR_REGION_ID_LIST:
-        first_color_prefix = "\033[1;33m"
-    elif system_data.get("region_id") in GALLENTE_REGION_ID_LIST:
-        first_color_prefix = "\033[1;32m"
-    elif system_data.get("region_id") in MINMATAR_REGION_ID_LIST:
-        first_color_prefix = "\033[1;31m"
-    else:
-        first_color_prefix = "\033[0m"
 
-    print(
-        f"{first_color_prefix}{system_name} located "
-        f"in {region_name}.{default_color_prefix}"
+def get_solar_system_data_dict() -> dict:
+    with open(
+        os.path.join(ESI_DATA_DIR, JSON_SOLAR_SYSTEMS_LIST_FILENAME)
+    ) as json_file:
+        solar_system_list: list[dict] = json.load(json_file)
+    solar_system_data_dict = {
+        system.get("name"): system for system in solar_system_list
+    }
+    return solar_system_data_dict
+
+
+def get_region_name_by_id(region_id: int) -> str:
+    return get_regions_data_dict().get(region_id, "")
+
+
+def get_solar_system_data(system_name: str) -> dict:
+    solar_system_dict = get_solar_system_data_dict().get(system_name)
+    if not solar_system_dict:
+        raise NameError(f"{system_name} not found.")
+    return solar_system_dict
+
+
+def get_solar_system_names_with_region_id(
+    solar_systems_list: list[str],
+) -> list[tuple]:
+    system_regions_list = []
+    for solar_system_name in solar_systems_list:
+        try:
+            system_data = get_solar_system_data(solar_system_name)
+        except NameError:
+            continue
+        else:
+            system_regions_list.append(
+                (solar_system_name, system_data.get("region_id"))
+            )
+    return system_regions_list
+
+
+def console_color_output_systems_list(systems_list: list) -> None:
+    default_color_prefix = "\033[0m"
+    for system_name, region_id in get_solar_system_names_with_region_id(
+        systems_list
+    ):
+        if region_id in CALDARI_REGION_ID_LIST:
+            first_color_prefix = "\033[1;36m"
+        elif region_id in AMARR_REGION_ID_LIST:
+            first_color_prefix = "\033[1;33m"
+        elif region_id in GALLENTE_REGION_ID_LIST:
+            first_color_prefix = "\033[1;32m"
+        elif region_id in MINMATAR_REGION_ID_LIST:
+            first_color_prefix = "\033[1;31m"
+        else:
+            first_color_prefix = "\033[0m"
+        print(
+            f"{first_color_prefix}{system_name} located "
+            f"in {get_region_name_by_id(region_id)}.{default_color_prefix}"
+        )
+
+
+if __name__ == "__main__":
+    console_color_output_systems_list(
+        random.choices([*get_solar_system_data_dict().keys()], k=5)
     )
